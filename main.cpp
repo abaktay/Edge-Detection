1f08e58eee2933c2b3ad7c603cc1093732c79e39
abaktay <abaktay1@gmail.com>
2026-05-28 22:55:45 +0200

    outputs edges, switched to vector

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include "inc/stb_image.h"
 #include "inc/stb_image_write.h"
 
-#define N 16
-float conv(const float mat[N][N], const float filter[3][3], int w, int h, int i, int j);
 
+float conv_(const std::vector<float>& mat, const float filter[3][3], int w, int h, int i, int j);
 int main()
 {
     int width, height, channels;
@@ -26,10 +26,7 @@ int main()
         return 1;
     }
 
-
-    // For a 16x16 image no need for heap allocation
-    float image[N][N];
-
+    std::vector<float> image_(height*width, 0.0f); 
     
     // Greyscale conversion formula per PMPP
     // L = r * 0.21 + g * 0.72 + b * 0.07
@@ -37,7 +34,7 @@ int main()
         for (int col = 0; col < width; col++) {
             int i = (row * width + col) * channels;
 
-            image[row][col] = data[i] * 0.21f + data[i+1] * 0.72f + data[i+2] * 0.07f;
+            image_[row*width + col] = data[i] * 0.21f + data[i+1] * 0.72f + data[i+2] * 0.07f;
         }
     } 
     stbi_image_free(data);
@@ -46,48 +43,46 @@ int main()
     const float Gx[3][3] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     const float Gy[3][3] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
     
-    float res_x[N][N];
-    float res_y[N][N];
+    std::vector<float> res_x_(height*width, 0.0f); 
+    std::vector<float> res_y_(height*width, 0.0f); 
 
     // Now, for all indices in image apply the 3x3 convolution
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-            res_x[row][col] = conv(image, Gx, width, height, row, col);
-            res_y[row][col] = conv(image, Gy, width, height, row, col);
+            res_x_[row*width+col] = conv_(image_, Gx, width, height, row, col);
+            res_y_[row*width+col] = conv_(image_, Gy, width, height, row, col);
         }
     }
 
-    float G[N][N];
-    stbi_uc out[N*N];
+    std::vector<float> G(width*height,0.0f);
+    std::vector<stbi_uc> out_(width*height, 0.0f);
 
     for (int row = 0; row < height; row++) {
         for (int col = 0; col < width; col++) {
-            G[row][col] = sqrtf((res_x[row][col]*res_x[row][col]) +(res_y[row][col]*res_y[row][col]));
-            // std::printf("%.2f\t",  G[row][col]);
+            G[row*width+col] = sqrtf((res_x_[row*width+col]*res_x_[row*width+col]) +(res_y_[row*width+col]*res_y_[row*width+col]));
 
-            out[row*width+col] =  (G[row][col] > 1000) ? 255 : 0;
+            out_[row*width+col] =  (G[row*width+col] > 100) ? 255 : 0;
         }
-        // std::cout << "\n";
     }
 
     stbi_write_png(
-        "edges.png",
+        "resources/edges.png",
         width,
         height,
         1,
-        out,
+        out_.data(),
         width
     );
     return 0;
 }
 
-float conv(const float mat[N][N], const float filter[3][3], int w, int h, int i, int j) {
+float conv_(const std::vector<float>& mat, const float filter[3][3], int w, int h, int i, int j) {
     float res = 0.0f;
     
     for (int dy = -1; dy <= 1; dy++) {
         for (int dx = -1; dx <= 1; dx++) {
             if (i+dy >= 0 && i+dy < h && j+dx >= 0 && j+dx < w)
-                res += filter[dy+1][dx+1] * mat[i+dy][j+dx];
+                res += filter[dy+1][dx+1] * mat[(i+dy)*w+ j+dx];
         }
     }
     return res;
